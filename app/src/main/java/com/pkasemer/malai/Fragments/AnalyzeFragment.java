8193407e3492b12771d7fa2853b692e2fe3e88ee
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2023-03-11 07:56:13 +0300

    image capture working

--- app/src/main/java/com/pkasemer/malai/Fragments/AnalyzeFragment.java
+++ app/src/main/java/com/pkasemer/malai/Fragments/AnalyzeFragment.java
@@ -1,66 +1,338 @@
 package com.pkasemer.malai.Fragments;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
+import androidx.exifinterface.media.ExifInterface;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.pkasemer.malai.MainActivity;
 import com.pkasemer.malai.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AnalyzeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
+
 public class AnalyzeFragment extends Fragment {
+    LinearLayout submit_btn;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    ImageView viewImage, take_picture;
+    String currentPhotoPath;
+    Uri currentpicURI;
+    ProgressBar progressBar;
+    View view;
+    private String[] PERMISSIONS;
     public AnalyzeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AnalyzeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AnalyzeFragment newInstance(String param1, String param2) {
-        AnalyzeFragment fragment = new AnalyzeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_analyze, container, false);
+
+        view = inflater.inflate(R.layout.fragment_analyze, container, false);
+        AskPermissions();
+        return view;
+    }
+
+    private void AskPermissions() {
+
+        PERMISSIONS = new String[]{
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.CAMERA,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        };
+
+        if(!hasPermissions(getContext(), PERMISSIONS)){
+            ActivityCompat.requestPermissions(getActivity(), PERMISSIONS, 1);
+        } else {
+            initViewPager(view);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 1){
+
+
+            if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(getContext(), "ACCESS_FINE_LOCATION granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getContext(), "ACCESS_FINE_LOCATION denied", Toast.LENGTH_SHORT).show();
+            }
+            if(grantResults[1]== PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(getContext(), "ACCESS_COARSE_LOCATION granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getContext(), "ACCESS_COARSE_LOCATION denied", Toast.LENGTH_SHORT).show();
+            }
+            if(grantResults[2]== PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(getContext(), "CAMERA granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getContext(), "CAMERA denied", Toast.LENGTH_SHORT).show();
+            }
+            if(grantResults[3]== PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(getContext(), "READ_EXTERNAL_STORAGEgranted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getContext(), "READ_EXTERNAL_STORAGE denied", Toast.LENGTH_SHORT).show();
+            }
+            if(grantResults[4]== PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(getContext(), "WRITE_EXTERNAL_STORAGE granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getContext(), "WRITE_EXTERNAL_STORAGE denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private boolean hasPermissions(Context context, String... PERMISSIONS){
+        if(context != null && PERMISSIONS != null){
+            for (String permission: PERMISSIONS){
+                if(ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+    private void initViewPager(View view) {
+        viewImage = view.findViewById(R.id.viewImage);
+        take_picture = view.findViewById(R.id.take_picture);
+        submit_btn = view.findViewById(R.id.submit_btn);
+        progressBar = view.findViewById(R.id.main_progress);
+        progressBar.setVisibility(View.GONE);
+
+        take_picture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        submit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadFile();
+            }
+        });
+
+    }
+
+    private void uploadFile() {
+        Toast.makeText(getContext(), "Upload btn", Toast.LENGTH_SHORT).show();
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "malAI_" + timeStamp + "_";
+        File storageDir = getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".png",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(requireContext().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(requireContext(),
+                        getString(R.string.fileprovider),
+                        photoFile);
+                currentpicURI = photoURI;
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                activityResultLauncher.launch(takePictureIntent);
+            }
+        }
+    }
+
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    try {
+                        setPic();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        getActivity().sendBroadcast(mediaScanIntent);
+    }
+
+    private void setPic() throws IOException {
+        // Get the dimensions of the View
+        int targetW = viewImage.getWidth();
+        int targetH = viewImage.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.max(1, Math.min(photoW/targetW, photoH/targetH));
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        viewImage.setImageBitmap(handleSamplingAndRotationBitmap(getContext(), contentUri));
+    }
+
+
+    public static Bitmap handleSamplingAndRotationBitmap(Context context, Uri selectedImage)
+            throws IOException {
+        int MAX_HEIGHT = 500;
+        int MAX_WIDTH = 500;
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        InputStream imageStream = context.getContentResolver().openInputStream(selectedImage);
+        BitmapFactory.decodeStream(imageStream, null, options);
+        imageStream.close();
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, MAX_WIDTH, MAX_HEIGHT);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        imageStream = context.getContentResolver().openInputStream(selectedImage);
+        Bitmap img = BitmapFactory.decodeStream(imageStream, null, options);
+
+        img = rotateImageIfRequired(context, img, selectedImage);
+        return img;
+    }
+
+
+    private static int calculateInSampleSize(BitmapFactory.Options options,
+                                             int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate ratios of height and width to requested height and width
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // Choose the smallest ratio as inSampleSize value, this will guarantee a final image
+            // with both dimensions larger than or equal to the requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+
+            final float totalPixels = width * height;
+
+            // Anything more than 2x the requested pixels we'll sample down further
+            final float totalReqPixelsCap = reqWidth * reqHeight * 2;
+
+            while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+                inSampleSize++;
+            }
+        }
+        return inSampleSize;
+    }
+
+
+    private static Bitmap rotateImageIfRequired(Context context, Bitmap img, Uri selectedImage) throws IOException {
+
+        InputStream input = context.getContentResolver().openInputStream(selectedImage);
+        ExifInterface ei;
+        if (Build.VERSION.SDK_INT > 23)
+            ei = new ExifInterface(input);
+        else
+            ei = new ExifInterface(selectedImage.getPath());
+
+        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                return rotateImage(img, 90);
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                return rotateImage(img, 180);
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                return rotateImage(img, 270);
+            default:
+                return img;
+        }
+    }
+
+    private static Bitmap rotateImage(Bitmap img, int degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        Bitmap rotatedImg = Bitmap.createBitmap(img, 0, 0, img.getWidth(), img.getHeight(), matrix, true);
+        img.recycle();
+        return rotatedImg;
     }
 }
