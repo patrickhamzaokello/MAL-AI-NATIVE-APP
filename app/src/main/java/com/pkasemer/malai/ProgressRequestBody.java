5e20799d1d70c1e61f812a3a5c1cc41a53aa0086
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2023-11-24 00:38:45 +0300

    fixed home layout of the app

---
+++ app/src/main/java/com/pkasemer/malai/ProgressRequestBody.java
@@ -0,0 +1,2 @@
+package com.pkasemer.malai;public class ProgressRequestBody {
+}
---
+++ app/src/main/java/com/pkasemer/malai/ProgressRequestBodyOne.java
@@ -0,0 +1,68 @@
+package com.pkasemer.malai;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import okio.Buffer;
+import okio.BufferedSink;
+import okio.ForwardingSink;
+import okio.Okio;
+import okio.Sink;
+
+import okio.Timeout;
+
+public class ProgressRequestBody extends RequestBody {
+
+    private final RequestBody requestBody;
+    private final ProgressListener listener;
+
+    public ProgressRequestBody(RequestBody requestBody, ProgressListener listener) {
+        this.requestBody = requestBody;
+        this.listener = listener;
+    }
+
+    @Override
+    public MediaType contentType() {
+        return requestBody.contentType();
+    }
+
+    @Override
+    public long contentLength() throws IOException {
+        return requestBody.contentLength();
+    }
+
+    @Override
+    public void writeTo(BufferedSink sink) throws IOException {
+        BufferedSink bufferedSink = Okio.buffer(sink(sink));
+        requestBody.writeTo(bufferedSink);
+        bufferedSink.flush();
+    }
+
+    private Sink sink(Sink sink) {
+        return new ForwardingSink(sink) {
+            private long bytesWritten = 0L;
+            private long contentLength = 0L;
+
+            @Override
+            public void write(Buffer source, long byteCount) throws IOException {
+                super.write(source, byteCount);
+
+                if (contentLength == 0) {
+                    contentLength = contentLength();
+                }
+
+                bytesWritten += byteCount;
+                listener.onProgress(bytesWritten, contentLength);
+
+            }
+        };
+    }
+
+    public interface ProgressListener {
+        void onProgress(long bytesWritten, long contentLength);
+    }
+}
